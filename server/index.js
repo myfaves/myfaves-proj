4105268b04a31cff01ccf25712cad98c277890d1
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-03-05 16:24:01 -0700

    added server/index.js boilerplate and installed initial server dependencies

--- server/index.js
+++ server/index.js
@@ -1,1 +1,38 @@
-const express = require('express')
+require("dotenv").config({ path: `${__dirname}/../.env` })
+const express = require("express")
+const massive = require("massive")
+const app = express()
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
+
+//CONTROLLERS
+// const authMid = require("./middleware/authMiddleware")
+
+//MIDDLEWARE
+app.use(express.json())
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
+  })
+)
+// app.use((req, res, next) => {
+//   console.log(`${new Date()} - ${req.method} request for ${req.url}`)
+// })
+
+//DB CONNECTION
+massive(CONNECTION_STRING).then(db => {
+  app.set('db', db)
+  console.log("Database Connected")
+  app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`))
+})
+
+//ENDPOINTS
+//AUTH ENDPOINTS
+// app.post("/auth/register", authCtrl.register)
+// app.post("/auth/login", authCtrl.login)
+// app.post("/auth/logout", authCtrl.logout)
+// app.get("/auth/user", authCtrl.getUser)
+
+//FAVS ENDPOINTS
