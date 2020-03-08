786aa2503dcdfad9f65804969b0d70202d6c812a
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-03-08 16:18:36 -0600

    basisc user profile functionality

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,22 +1,74 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import useCheckUser from '../hooks/useCheckUser'
+import React, { useState } from "react"
+import { connect } from "react-redux"
+import Input from "./Reusable/Input"
+import useCheckUser from "../hooks/useCheckUser"
+import useInput from "../hooks/useInput"
 
-const UserProfile = ({user, history}) => {
+const UserProfile = ({ user, history }) => {
   useCheckUser(user, history.push)
-  const {first_name, last_name, age, email} = user
+  const [{ first_name, last_name, age, email }, setValues] = useInput({
+    first_name: user.first_name,
+    last_name: user.last_name,
+    age: user.age,
+    email: user.email
+  })
+  const [editToggle, setEditToggle] = useState(false)
+  const updateProfile = e => {
+    e.preventDefault()
+    console.log(
+      `First Name: ${first_name}, Last Name: ${last_name}, Email: ${email}, Age: ${age}`
+    )
+    //submit functionality to come
+  }
   return (
     <div>
-      <h1>Name: {first_name} {last_name}</h1>
-      <h1>Age: {age}</h1>
-      <h1>Email: {email}</h1>
+      <button onClick={() => setEditToggle(!editToggle)}>
+        {!editToggle ? "Edit" : "Cancel"}{" "}
+      </button>
+      {!editToggle ? (
+        <div>
+          <h1>
+            Name: {first_name} {last_name}
+          </h1>
+          <h1>Age: {age}</h1>
+          <h1>Email: {email}</h1>
+        </div>
+      ) : (
+        <form onSubmit={updateProfile}>
+          <Input
+            name="first_name"
+            value={first_name}
+            placeHolder="First Name"
+            handleChange={setValues}
+          />
+          <Input
+            name="last_name"
+            value={last_name}
+            placeHolder="Last Name"
+            handleChange={setValues}
+          />
+          <Input
+            name="age"
+            value={age}
+            placeHolder="Age"
+            handleChange={setValues}
+          />
+          <Input
+            name="email"
+            value={email}
+            placeHolder="Email"
+            handleChange={setValues}
+          />
+          <button type="submit">Save</button>
+        </form>
+      )}
     </div>
   )
 }
 
-const mapStateToProps = (store) => {
-  const {user} = store.reducer
-  return {user}
+const mapStateToProps = store => {
+  const { user } = store
+  return { user }
 }
 
 export default connect(mapStateToProps)(UserProfile)
