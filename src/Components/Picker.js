67417fdd904d2696feca0370a58a2d4481a5c648
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-03-09 15:10:46 -0600

    random picker changes

--- src/Components/Picker.js
+++ src/Components/Picker.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
+import SearchBar from "./SearchBar"
+import MovieList from "./MovieList"
+import axios from 'axios'
 
-const Picker = (props) => {
+const Picker = props => {
+  const {REACT_APP_MOVIE, REACT_APP_GAME} = process.env
+  const [searchMovies, setSearchMovies] = useState("")
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    getGames()
+  }, [])
+  const submitMovies = e => {
+    e.preventDefault()
+    fetch(
+      `https://api.themoviedb.org/3/search/movie/?api_key=${REACT_APP_MOVIE}&query=${searchMovies}`
+    )
+      .then(data => data.json())
+      .then(data => setMovies(data.results))
+      .catch(err => console.log(err))
+  }
+  
   return (
-    <div>Picker</div>
+    <div>
+      <SearchBar handleSubmit={submitMovies} handleChange={setSearchMovies} />
+      <MovieList movies={movies} />
+    </div>
   )
 }
 
