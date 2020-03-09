da6418aee27b68011aaab8a725b4f281942e2c85
Tommy Manyvanh <tommy_manyvanh@yahoo.com>
2020-03-09 16:27:19 -0600

    fixed api key

--- src/Components/CategoryList.js
+++ src/Components/CategoryList.js
@@ -18,14 +18,14 @@ const CategoryList = (props) => {
       <div className="gamelist-container">
         {
           props.games.map((game, i) => {
-            // console.log(game[i])
             return (
-              <div key={i}>
-                <div className="game-container">
+              <div className="game-container"key={i}>
+                <div>
             {
               game.background_image == null ? <img src={'https://jschof.com/wp-content/uploads/2019/10/output-onlinepngtools-1024x943.png'} alt="card-three" style={{width: 300, height: 250}} ></img> : <img src={game.background_image} alt="card-four" style={{width: 300, height: 250}}/> 
             }
                 </div>
+                <div>{game.name}</div>
               </div>
             )
           })
