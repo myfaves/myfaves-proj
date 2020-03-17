3df8e4e9ac9d4eb114f3967ef4db7891fe9341a2
Nick Rosa <nicholascastrorosa@gmail.com>
2020-03-17 14:02:31 -0600

    style

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -9,6 +9,12 @@ import ShowModal from "../Components/Modals/ShowModal"
 
 const Movie = ({ image, body, msg }) => {
   const [modal, setModal] = useState(false)
+
+  const getFavorites = () => {
+    axios
+      .get('/api/favorites')
+  }
+
   const addFavorite = () => {
     axios
       .post("/api/favorites", body)
@@ -17,44 +23,32 @@ const Movie = ({ image, body, msg }) => {
       })
       .catch(err => console.log(err))
   }
-  
-  const getFavorites = () => {
-    axios
-    .get('/api/favorites')
-  }
 
-  const removeFavorite = () => {
-    axios
-      .delete(`/api/favorites/${favorite_id}`, body)
-      .then(res => {
-        toast.success(msg)
-      })
-      .catch(err => console.log(err))
-  }
+  // const removeFavorite = () => {
+  //   axios
+  //     .delete(`/api/favorites/${favorite_id}`)
+  //     .then(res => {
+  //       toast.success(msg)
+  //     })
+  //     .catch(err => console.log(err))
+  // }
 
   const closeModal = e => {
     e.stopPropagation()
     setModal(false)
   }
 
-
-
   return (
     <div className="card-container">
       <div className="card-container-top">
-        <div class="click">
-          <span class="fa fa-star-o"></span>
-          <div class="ring"></div>
-          <div class="ring2"></div>
-          <p class="info">Added to favourites!</p>
-        </div>
+        <svg 
+        // addFavorite 
+        id="add-favorite"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>
         <div
           className="card-image"
           onClick={e => {
             closeModal(e)
             setModal(true)
-
-            // addFavorite
           }}
         >
           {!image ? (
