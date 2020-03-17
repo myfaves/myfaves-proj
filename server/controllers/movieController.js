2c23cb57757923c33d35318aabe989e1623b9203
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-03-17 13:52:36 -0600

    friends functionality frontend

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,36 +1,36 @@
-module.exports = {
-  getFavorites: (req, res) => {
-    const db = req.app.get("db")
-    const { user_id } = req.session.user
-    db.movie
-      .get_favorites(user_id)
-      .then(results => res.status(200).send(results))
-      .catch(err => res.status(500).send(err))
-  },
-  addFavorite: (req, res) => {
-    const db = req.app.get("db")
-    const { user_id } = req.session.user
-    const { name, image, rating, external_id } = req.body
-    const category_id = 1
-    db.movie
-      .add_favorite({
-        user_id,
-        category_id,
-        name,
-        image,
-        rating,
-        external_id
-      })
-      .then(results => res.status(200).send(results))
-      .catch(err => res.status(500).send(err))
-  },
-  deleteFavorite: (req, res) => {
-    const db = req.app.get("db")
-    const { user_id } = req.session.user
-    const { movie_id } = req.params
-    db.movie
-      .delete_favorite(user_id, movie_id)
-      .then(results => res.status(200).send(results))
-      .catch(err => res.status(500).send(err))
-  }
-}
+// module.exports = {
+//   getFavorites: (req, res) => {
+//     const db = req.app.get("db")
+//     const { user_id } = req.session.user
+//     db.movie
+//       .get_favorites(user_id)
+//       .then(results => res.status(200).send(results))
+//       .catch(err => res.status(500).send(err))
+//   },
+//   addFavorite: (req, res) => {
+//     const db = req.app.get("db")
+//     const { user_id } = req.session.user
+//     const { name, image, rating, external_id } = req.body
+//     const category_id = 1
+//     db.movie
+//       .add_favorite({
+//         user_id,
+//         category_id,
+//         name,
+//         image,
+//         rating,
+//         external_id
+//       })
+//       .then(results => res.status(200).send(results))
+//       .catch(err => res.status(500).send(err))
+//   },
+//   deleteFavorite: (req, res) => {
+//     const db = req.app.get("db")
+//     const { user_id } = req.session.user
+//     const { movie_id } = req.params
+//     db.movie
+//       .delete_favorite(user_id, movie_id)
+//       .then(results => res.status(200).send(results))
+//       .catch(err => res.status(500).send(err))
+//   }
+// }
